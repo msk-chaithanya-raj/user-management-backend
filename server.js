8699ee6ad28f13a62e10246c11d12e601921268c
Sri Krishna Chaithanya Raj Masimukku <mskchaithanyaraj@gmail.com>
2025-01-26 19:30:45 +0530

    backend v2

--- server.js
+++ server.js
@@ -9,7 +9,15 @@ const bcrypt = require("bcryptjs");
 const app = express();
 const PORT = process.env.PORT || 5000; // Use env variable or fallback to 5000
 
-app.use(cors());
+// Enable CORS for all origins or specify origins you want to allow
+app.use(
+  cors({
+    origin: ["http://localhost:5173", ""], // Allow the frontend origin(s)
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
+  })
+);
+
 app.use(express.json());
 
 // MongoDB connection using the DB_URI environment variable
